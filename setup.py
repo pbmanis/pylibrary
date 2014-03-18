b2a7405e52a159600cf6a5d13c04f53a95db6a75
Paul B. Manis <pmanis@med.unc.edu>
2014-03-18 13:05:34 -0400

    Added missing setup.py to repository added missing files for chebfit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pylibrary',
+      version='0.12',
+      description='Paul Manis,  Python Utilities',
+      url='',
+      author='Paul B. Manis, Ph.D.',
+      author_email='pmanis@med.unc.edu',
+      license='MIT',
+      packages=['pylibrary', 'pylibrary.xlsx'],
+      zip_safe=False)
