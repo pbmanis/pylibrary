f2e1d9717aa022415465b2fdefcda646fb0df64b
Paul manis <pmanis@med.unc.edu>
2016-05-03 08:56:09 -0400

    Standardized docstrings; cleaned up error handling, added paired tests to t-test and rank-sums

--- pylibrary/RStats.py
+++ pylibrary/RStats.py
@@ -2,8 +2,17 @@
 # encoding: utf-8
 """
 RStats.py
-Wrappers for using R for data anlsysis
+Wrappers for using R, scipy and a custom permutation routine for data anlsysis
+
 Currently implements 1-way ANOVA for 3, 4, and 5 groups with all-way posttests
+t-tests (paired, unpaired, equal and unequal variance)
+    Runs 
+non-parametric tests
+permutation 
+
+To use:
+import pylibrary.RStats as RStats
+
 Created by Paul Manis on 2014-06-26.
 
 Copyright 2010-2014  Paul Manis
@@ -11,6 +20,7 @@ Distributed under MIT/X11 license. See license.txt for more infofmation.
 """
 import scipy.stats as Stats
 import numpy as np
+
 
 # connect to R via RPy2
 try:
@@ -31,13 +41,28 @@ except:
 def OneWayAnova(dataDict=None, dataLabel='data', mode='parametric'):
     """
     Perform a one way ANOVA with N groups
-    :param dataDict: Dictionary holding data in format
-        {'group1': [dataset], 'group2': [dataset], 'group3': [dataset]}.
-    :param dataLabel: title to use for print out of data
-    :param mode: 'parametric' (default) uses R aov. anything else uses Kruskal-Wallis
+    
+    Parameters
+    ----------
+    dataDict : Dictionary holding data (default: None)
+        Format: {'group1': [dataset], 'group2': [dataset], 'group3': [dataset]}.
+    dataLabel : string (default: 'data')
+        title to use for print out of data
+    mode : string (default: 'parametric')
+        Choices: 'parametric' uses aov from R
+                 'nonparametric' uses Kruskal-Wallis from R
     Prints resutls and post-hoc tests...
-    :return: Nothing
+    
+    Returns
+    -------
+    Nothing
     """ 
+    # test calling values
+    if mode not in ['parametric', 'nonparametric']:
+        raise ValueError('RStats.OneWayAnova: Mode must be either parametric or nonparametric; got %s' % mode)
+    if dataDict is None or not isinstance(dataDict, dict) or len(dataDict.keys()) < 2:
+        raise ValueError('RStats.OneWayAnova: dataDict must be a dictionary with at least 2 keys')
+    
     labels = dataDict.keys()
     NGroups = len(labels)
     if NGroups <= 2:
@@ -96,7 +121,7 @@ def OneWayAnova(dataDict=None, dataLabel='data', mode='parametric'):
         print 'Post Hoc (Tukey HSD): '
         y=robjects.r("TukeyHSD(aov)")
         print y
-    else:
+    if mode == 'nonparametric':
         kw = robjects.r("kruskal.test(Yd ~ Equation, data=vData)")
         print kw
         loc = robjects.r('Equation=factor(rep(c(%s), times=c(%s)))' % (clargs, clenargs))
@@ -104,20 +129,19 @@ def OneWayAnova(dataDict=None, dataLabel='data', mode='parametric'):
         print "PostTest: pairwise wilcox\n", kwmc
 
 
-
 def permTS(dataDict=None, dataLabel='data', mode='exact.ce'):
     """
-    perform a two-sample permutation test using the perm package in R
-    Uses the monte-carlo simulation method - not necessarily the fastest, but is "exact"
+    permTS performs a two-sample permutation test using the 'perm' package in R
+    Uses the Monte-Carlo simulation method - not necessarily the fastest, but is "exact"
     This routine is a wrapper for permTS in R.
     
     Params
     ------
-    dataDict: Dictionary holding data in format
-        {'group1': [dataset], 'group2': [dataset]}.
-    dataLabel: str
+    dataDict: Dictionary
+        Data format {'group1': [dataset], 'group2': [dataset]}.
+    dataLabel: string
         title to use for print out of data
-    mode: str
+    mode: string
         test mode (see manual. Usually 'exact.ce' for "complete enumeration", or 
         'exact.mc' for montecarlo)
     Returns
@@ -125,11 +149,15 @@ def permTS(dataDict=None, dataLabel='data', mode='exact.ce'):
     (p, n) : tuple
         p value for test (against null hypothesis), and n, number of mc replications or 0 for other tests
     """
+    
+    # test calling values
+    if mode not in ['exact.ce', 'exact.mc']:
+        raise ValueError('RStats.permTS: Mode must be either "exact.ce" or "exact.mc"; got %s' % mode)
+    if dataDict is None or not isinstance(dataDict, dict) or len(dataDict.keys()) != 2:
+        raise ValueError('RStats.OneWayAnova: dataDict must be a dictionary with exactly 2 keys')
+
     labels = dataDict.keys()
     NGroups = len(labels)
-    if NGroups != 2:
-        print "Need at exactly 2 groups to run Two Sample permutation test (permTS)"
-        return
     cmdx = 'X=c(%s)' % ', '.join(str(x) for x in dataDict[labels[0]])
     cmdy = 'Y=c(%s)' % ', '.join(str(y) for y in dataDict[labels[1]])
     importr('perm')
@@ -151,13 +179,33 @@ def permTS(dataDict=None, dataLabel='data', mode='exact.ce'):
         print(u'  p={:8.6f}, Nperm={:8d} [mode={:s}]'.format(float(pvalue), int(nmc), mode))
     return (pvalue, nmc)  # return the p value for this test, and the number of mc replicatess
 
+
 def permutation(data, dataLabel=None, nperm=10000, decimals=4):
     """
-    Brute force permutation test
+    Brute force permutation test.
+    
+    Parameters
+    ------
+    dataDict : Dictionary
+        Data format {'group1': [dataset], 'group2': [dataset]}.
+    dataLabel : string
+        title to use for print out of data
+    nperm : int (default 10000)
+        Number of permutation trials to run to calculate probabilities
+    decimals : int (default 4)
+        decimals in formatted printout
+    Returns
+    -------
+    (p, n) : tuple
+        p value for test (against null hypothesis), and n, number permutations run
     """
+    
+    # test calling values
+    if data is None or not isinstance(data, dict) or len(data.keys()) != 2:
+        raise ValueError('RStats.permutation: data must be a dictionary with at exactly 2 keys' +
+            '\nUse KW (anova) for more than 2 groups')
+
     k = data.keys()
-    if len(k) > 2:
-        raise ValueError('Permutation can only compare 2 groups: use KW (anova) for more than 2 groups')
         
     g1 = data[k[0]]
     g2 = data[k[1]]
@@ -193,19 +241,39 @@ def permutation(data, dataLabel=None, nperm=10000, decimals=4):
         # iqr2 = np.subtract(*np.percentile(g2, [75, 25]))
         # print(u'  {:s}: median={:8.4f}  IQR={:8.4f}'.format(k[0].rjust(n), np.median(g1), iqr1))
         # print(u'  {:s}: median={:8.4f}  IQR={:8.4f}'.format(k[1].rjust(n), np.median(g2), iqr2))
-        print(u'  Observed difference: {:8.4f}\n'.format(diffobs))
-        print(u'  p={:8.6f}, Nperm={:8d}'.format(float(pvalue), int(nperm)))
+        print(u'  Observed difference: {:8.4f}'.format(diffobs))
+        print(u'  p={:8.6f}, Nperm={:8d}\n'.format(float(pvalue), int(nperm)))
     return(pvalue, nperm)
 
-def ttest(data, dataLabel=None, decimals=4):
+
+def ttest(data, dataLabel=None, paired=False, decimals=4):
     """
-    perform a t-test using Scipy.stats
-    :param data: dictionary {'g1': [data], 'g2': [data]}
-    :param dataLabel: string with label to be printed for summary information
-    :return df: degrees of freedom calculated assuming unequal variance
-    :return t: t statistic for the difference
-    :return p: p value
+    Perform a t-test using Scipy.stats
+    
+    Parameters
+    ----------
+    data : Dictionary (default: None)
+        Data format {'group1': [dataset], 'group2': [dataset]}.
+    dataLabel : string (default: None)
+        title to use for print out of data
+    paired : Boolean (default: False)
+        Set true to do "paired" t-test, false to do unpaired (independent samples) test.
+    decimals : int (default 4)
+        decimals in formatted printout
+    
+    Returns
+    -------
+     (df, t, p) : tuple
+        df : degrees of freedom calculated assuming unequal variance
+        t : t statistic for the difference
+        p : p value
     """
+    
+    # test calling values
+    if data is None or not isinstance(data, dict) or len(data.keys()) != 2:
+        raise ValueError('RStats.permutation: data must be a dictionary with at exactly 2 keys' +
+            '\nUse KW (anova) for more than 2 groups')
+            
     k = data.keys()
     g1 = data[k[0]]
     g2 = data[k[1]]
@@ -218,7 +286,10 @@ def ttest(data, dataLabel=None, decimals=4):
         equalVar = True
     else:
         equalVar = False
-    (t, p) = Stats.ttest_ind(g1, g2, equal_var=equalVar)
+    if paired:
+        (t, p) = Stats.ttest_rel(g1, g2, equal_var=equalVar)
+    else:
+        (t, p) = Stats.ttest_ind(g1, g2,)
     g1mean = np.mean(g1)
     g1std = np.std(g1)
     g2mean = np.mean(g2)
@@ -227,8 +298,11 @@ def ttest(data, dataLabel=None, decimals=4):
     # tN[k])**2 / (tN[k] - 1) ) + ( (dstd[k]**2 / dN[k])**2 / (tN[k] - 1) ) )
     df = (g1std**2/n1 + g2std**2/n2)**2 / (((g1std**2 / n1)**2 / (n1 - 1) + ((g2std**2 / n2)**2 / (n1 - 1))))
     if dataLabel is not None:
+        testtype = 'Independent'
+        if paired:
+            testtype = 'Paired'
         n = max([len(l) for l in k])
-        print '\nT-test, data set = %s' % (dataLabel)
+        print '\n%s T-test, data set = %s' % (testtype, dataLabel)
         if p1 < 0.05 and p2 < 0.05:
             print(u'  Both data sets appear normally distributed: Shapiro-Wilk Group 1 p = {:6.3f}, Group2 p = {:6.3f}'.format(p1, p2))
         else:
@@ -240,24 +314,48 @@ def ttest(data, dataLabel=None, decimals=4):
             print(u'  Variances are unequal (Bartletts test, p = {:.3f}); not assuming equal variances'.format(pb))
         print(u'  {:s}={:8.{pc}f}\u00B1{:.{pc}f}  (mean, SD)'.format(k[0].rjust(n), g1mean, g1std, pc=decimals))
         print(u'  {:s}={:8.{pc}f}\u00B1{:.{pc}f}  (mean, SD)'.format(k[1].rjust(n), g2mean, g2std, pc=decimals))
-        print(u'  t({:6.2f})={:8.4f}   p={:8.6f}'.format(df, float(t), float(p)))
+        print(u'  t({:6.2f})={:8.4f}   p={:8.6f}\n'.format(df, float(t), float(p)))
     return(df, float(t), float(p))
 
-def ranksums(data, dataLabel=None, decimals=4):
+
+def ranksums(data, dataLabel=None, paired=False, decimals=4):
     """
-    perform a t-test using Scipy.stats
-    :param data: dictionary {'g1': [data], 'g2': [data]}
-    :param dataLabel: string with label to be printed for summary information
-    :return df: degrees of freedom calculated assuming unequal variance
-    :return t: t statistic for the difference
-    :return p: p value
+    Perform a rank sums test using Scipy.stats
+    Use for non-parametric data sets 
+    
+    Parameters
+    ----------
+    data : Dictionary (default: None)
+        Data format {'group1': [dataset], 'group2': [dataset]}.
+    dataLabel : string (default: None)
+        title to use for print out of data
+    paired : Boolean (default: False)
+        Set true to do "paired" t-test, false to do unpaired (independent samples) test.
+    decimals : int (default 4)
+        decimals in formatted printout
+    
+    Returns
+    -------
+     (df, t, p) : tuple
+        df : degrees of freedom calculated assuming unequal variance
+        t : t statistic for the difference
+        p : p value
     """
+    if data is None or not isinstance(data, dict) or len(data.keys()) != 2:
+        raise ValueError('RStats.permutation: data must be a dictionary with at exactly 2 keys' +
+            '\nUse KW (anova) for more than 2 groups')
+
     k = data.keys()
     g1 = data[k[0]]
     g2 = data[k[1]]
     n1 = len(g1)
     n2 = len(g2)
-    (z, p) = Stats.ranksums(g1, g2)
+    if paired:
+        (z, p) = Stats.wilcoxon(g1, g2)
+        testtype = "Wilcoxon singed-rank"
+    else:
+        (z, p) = Stats.ranksums(g1, g2)
+        testtype = "Rank-sums test"
     g1mean = np.mean(g1)
     g1std = np.std(g1)
     g2mean = np.mean(g2)
@@ -266,7 +364,7 @@ def ranksums(data, dataLabel=None, decimals=4):
     (w2, p2) = Stats.shapiro(g2, a=None, reta=False)
     if dataLabel is not None:
         n = max([len(l) for l in k])
-        print '\nWilcoxon Rank Sums test, data set = %s' % dataLabel
+        print '\n%s test, data set = %s' % (testtype, dataLabel)
         if p1 < 0.05 and p2 < 0.05:
             print(u'  Both data sets appear normally distributed: Shapiro-Wilk Group 1 p = {:6.3f}, Group2 p = {:6.3f}'.format(p1, p2))
         else:
@@ -280,7 +378,7 @@ def ranksums(data, dataLabel=None, decimals=4):
         # iqr2 = np.subtract(*np.percentile(g2, [75, 25]))
         # print(u'  {:s}: median={:8.4f}  IQR={:8.4f}'.format(k[0].rjust(n), np.median(g1), iqr1))
         # print(u'  {:s}: median={:8.4f}  IQR={:8.4f}'.format(k[1].rjust(n), np.median(g2), iqr2))
-        print(u'  z={:8.4f}   p={:8.6f}'.format(float(z), float(p)))
+        print(u'  z={:8.4f}   p={:8.6f}\n'.format(float(z), float(p)))
     return(float(z), float(p))    
 
 
@@ -297,11 +395,22 @@ def summarizeData(data, dataLabel=None, decimals=4):
         print(u'  {:s}:  {:8.{pc}f}, {:.{pc}f} (median, IQR)'.format(k.rjust(n), np.median(g1), iqr1, pc=decimals))    
 
 
-def test(ngroups=3):
+def testANOVA():
     """
     Tests for anova routines
     3 group anova taken from Prism 6.01 example
+    Prints results for direct comparisoin with the Prism output, which
+    is copied below.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    Nothing
     """
+    
     data={'Control': [54, 23, 45, 54, 45, 47], 'Treated': [87, 98, 64, 77, 89], 
     'TreatedAntagonist': [45, 39, 51, 49, 50, 55]}
     OneWayAnova(dataDict=data, dataLabel='3Groups', mode='parametric')
@@ -359,26 +468,44 @@ def test(ngroups=3):
 
     """
 
+
 def test2Samp():
+    """
+    Perform tests on ranksums, permutation permTS and t-test, all on 
+    2 samples. Runs using a fixed random number seed (0) each time
+    Prints results for each test.
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    Nothing. 
+    """
 
     sigmax = 1.0
     sigmay = 3.0
     mux = 0.0
     muy = 3.0
     nx = 10
-    ny = 8
+    ny = 10
     np.random.RandomState(0)  # set seed to 0
-    datax = sigmax * np.random.randn(nx) + mux   
-    datay = sigmay * np.random.randn(ny) + muy   
+    datax = sigmax * np.random.randn(nx) + mux
+    datay = sigmay * np.random.randn(ny) + muy
     datadict = {'x': datax, 'y': datay}
-    ranksums(datadict, dataLabel='Test Rank Sums')
-    permutation(datadict, dataLabel='Test simple permute')
-    ttest(datadict, dataLabel='Standard t-test')
+    ranksums(datadict, dataLabel='Test Rank Sums (scipy)')
+    ranksums(datadict, dataLabel='Test Rank Sums, Paired (scipy)', paired=True)
+    ttest(datadict, dataLabel='Standard t-test (scipy)')
+    ttest(datadict, dataLabel='Standard t-test (scipy), paired')
     (p, n) = permTS(datadict, dataLabel='R permTS')
+    permutation(datadict, dataLabel='Test simple permute')
 
-    
-    
+
 if __name__ == '__main__':
-    #test()
+    """
+    If called from commmand line, runs tests on routines
+    """
+    testANOVA()
     test2Samp()
     
