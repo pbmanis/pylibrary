f3fb170a55ebc5db38606efa59eec695a8d7db1a
Paul manis <pmanis@med.unc.edu>
2020-01-10 16:39:29 -0500

    add colormaps files to library

---
+++ pylibrary/plotting/colormaps/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from ..colormaps import option_a
+from ..colormaps import option_a
+from ..colormaps import option_b
+from ..colormaps import option_c
+from ..colormaps import option_d
+from ..colormaps import RdBuGnYl_r_v2
+from ..colormaps import blu_grn_pnk2
+from ..colormaps import parula
+from ..colormaps import PuBuGnYl_r
+from ..colormaps import RdBuGnYl_r
+from ..colormaps import fake_parula
