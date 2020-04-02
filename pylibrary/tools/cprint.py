f16bcbe490e8a11d99792dfd65d3ed18a0cd6756
pbmanis <pmanis@med.unc.edu>
2020-04-02 15:57:21 -0400

    added single char codes to cprint

--- pylibrary/tools/cprint.py
+++ pylibrary/tools/cprint.py
@@ -3,8 +3,14 @@ Routine to perform color printing of text, wrapping standard pyton print functio
 
 """
 # ANSI terminal colors  - just put in as part of the string to get color terminal output
-colors = {'red': '\x1b[31m', 'yellow': '\x1b[33m', 'green': '\x1b[32m', 'magenta': '\x1b[35m',
-              'blue': '\x1b[34m', 'cyan': '\x1b[36m' , 'white': '\x1b[0m', 'backgray': '\x1b[100m'}
+colors = {'red': '\x1b[31m', 'r': '\x1b[31m', 
+          'yellow': '\x1b[33m', 'y': '\x1b[33m',
+          'green': '\x1b[32m', 'g': '\x1b[32m', 
+          'magenta': '\x1b[35m', 'ma': '\x1b[35m',
+          'blue': '\x1b[34m', 'b': '\x1b[34m',
+          'cyan': '\x1b[36m' , 'c': '\x1b[36m' ,
+          'white': '\x1b[0m', 'w': '\x1b[0m',
+          'backgray': '\x1b[100m'}
 
 def cprint(c, txt, default='white', **kwds):
     """
