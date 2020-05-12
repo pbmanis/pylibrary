3a51047a6ca683638efc9e64b49023d5806dc184
pbmanis <pmanis@med.unc.edu>
2020-05-12 14:15:50 -0400

    switch to time.perf_counter as time.clock will be deprecated

--- pylibrary/tools/ptime.py
+++ pylibrary/tools/ptime.py
@@ -10,19 +10,21 @@ import sys
 import time as systime
 START_TIME = None
 time = None
-
+#time.perf_counter or time.process_time instead
 def winTime():
     """Return the current time in seconds with high precision (windows version, use Manager.time() to stay platform independent)."""
-    return systime.clock() + START_TIME
-    #return systime.time()
+    # return systime.clock() + START_TIME
+    return systime.perf_counter() + START_TIME
+    TIME#return systime.time()
 
 def unixTime():
     """Return the current time in seconds with high precision (unix version, use Manager.time() to stay platform independent)."""
     return systime.time()
 
 if 'win' in sys.platform:
-    cstart = systime.clock()  ### Required to start the clock in windows
-    START_TIME = systime.time() - cstart
+    #cstart = systime.clock()  ### Required to start the clock in windows
+    START_TIME = systime.perf_counter()
+    #START_TIME = systime.time() - cstart
     
     time = winTime
 else:
