0a87ddf71daf9b4bcd037f8feac39292cf7e6b4b
Paul Manis <pmanis@med.unc.edu>
2025-03-27 15:35:43 -0400

    re-enable submodules

--- pylibrary/__init__.py
+++ pylibrary/__init__.py
@@ -3,8 +3,8 @@ __author__ = "Paul B. Manis"
 __version__ = '0.4.3'
 
 # submodules
-# import pylibrary.fitting
-# import pylibrary.plotting
-# import pylibrary.stats
-# import pylibrary.tools
+import pylibrary.fitting
+import pylibrary.plotting
+import pylibrary.stats
+import pylibrary.tools
 
